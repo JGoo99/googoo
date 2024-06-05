c8251d9ff7223b2a20193c715abc324012c3116b
JGoo99 <zzgg3040@naver.com>
2024-06-06 03:53:58 +0900

    first-commit

---
+++ src/main/java/com/gg/googoo/GoogooApplication.java
@@ -0,0 +1,13 @@
+package com.gg.googoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogooApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoogooApplication.class, args);
+  }
+
+}
